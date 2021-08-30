f95b7b77d2037db726fea6b20d395c7a6843d003
rdilare <rdandroidunt@gmail.com>
2021-08-30 23:54:10 +0530

    added bootstrap4.5

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ songs_db = Songdb()
 
 app = Flask(__name__)
 
+@app.route("/base")
+def base():
+	return render_template("base.html")
+
 @app.route("/")
 def index():
 	return render_template("index.html",songs_data = songs_db.getData())
@@ -19,3 +23,7 @@ def addSong():
 	print(f"added song details-\nname: {data['name']}\nartist: {data['artist']}")
 	songs_db.addSong(name,artist)
 	return render_template("index.html",songs_data = songs_db.getData())
+
+
+if __name__=="__main__":
+	app.run(debug=True)
