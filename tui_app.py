38da7aa820ed3af1b5ae3773c951978d24acc2fe
rdilare <rahul@xyz.abc>
2023-07-02 18:11:14 +0530

    rd : truncating the long song name

--- tui_app.py
+++ tui_app.py
@@ -27,6 +27,12 @@ def getId(id_name_string):
     return id
 
 
+def truncString(s, width):
+    assert width>0, "width should be greater than zero."
+    if len(s) > width:
+        s = s[0:width] + "....."
+    return s
+
 
 
 class Tui:
@@ -34,6 +40,8 @@ class Tui:
         self.included_tags = []
         self.excluded_tags = []
         self.data_handler = JH()
+        self.max_string_length = 50
+
         self.actions_dict = {
                     "Include Tags" : self.includeTags,
                     "Exclude Tags" : self.excludeTags,
@@ -41,7 +49,7 @@ class Tui:
                     "Add Tags to Song" : self.addTagsToSong,
                     "Remove Tags From Song" : self.removeTagsFromSong,
                     "Exit" : self.quit
-        }
+                    }
 
     def listSongs(self):
         # songs = self.data_handler.get_songs()
@@ -50,7 +58,7 @@ class Tui:
 
         print(f"\n____________________Song List____________________")
         for item in idWithName(songs)[:10]:
-            print(item)
+            print(truncString(item, self.max_string_length))
         print(f"_________________________________________________\n")
         print(f"Included Tags: {self.included_tags}\t Excluded Tags: {self.excluded_tags}")
         # print(f".................................................")
@@ -64,9 +72,7 @@ class Tui:
                 ).execute()
 
         song_id = getId(song)
-
         tags_in_song = self.data_handler.get_songs()[str(song_id)]["tags"]
-
         print(f"Current Tags: {tags_in_song}")
 
         tags = inquirer.select(
@@ -77,15 +83,13 @@ class Tui:
         
         if "Add New Tag" in tags:
             new_tag = inquirer.text(
-                message = "New Tag: ",
-            ).execute()
-
+                    message = "New Tag: ",
+                    ).execute()
             tags.remove("Add New Tag")
             tags.append(new_tag)
 
         self.data_handler.add_tags(str(song_id), tags)
         print("[debug]: ",self.data_handler.get_songs()[str(song_id)])
-
 
     def removeTagsFromSong(self):
         song = inquirer.fuzzy(
@@ -106,8 +110,6 @@ class Tui:
 
         self.data_handler.remove_tags_from_song(str(song_id), tags)
         print("[debug]: ",self.data_handler.get_songs()[str(song_id)])
-
-
         
     def includeTags(self):
         tags = inquirer.checkbox(
@@ -128,7 +130,7 @@ class Tui:
             # validate=lambda result: len(result) >= 1,
             # invalid_message="should be at least 1 selection",
             # instruction="(select at least 1)",
-        ).execute()
+            ).execute()
         self.excluded_tags = tags
 
     def viewSongInfo(self):
@@ -136,22 +138,20 @@ class Tui:
                 message="Select Song: ",
                 choices=idWithName(self.data_handler.get_songs().values()),
                 default="",
+                transformer = lambda x : truncString(x, self.max_string_length)
                 ).execute()
 
         song_id = getId(song)
-
         song_data = self.data_handler.get_songs()[str(song_id)]
-        print(f"Song: {song_data['name']}")
-        print(f"Artist: {song_data['artist']}")
-        print(f"Tags: {song_data['tags']}")
+        print(f"Song\t: {truncString(song_data['name'], self.max_string_length)}")
+        print(f"Artist\t: {song_data['artist']}")
+        print(f"Tags\t: {song_data['tags']}")
 
     def quit(self):
         return "exit"
     
     def getMainOptions(self):
-        return self.actions_dict.keys()
-
-    
+        return self.actions_dict.keys()  
 
     def runApp(self):
         while True:
